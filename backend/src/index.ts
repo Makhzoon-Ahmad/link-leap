ca915e650fc732662dd989c65dda34b64b05f9ba
Makhzoon <amakhzoon10@gmail.com>
2025-03-21 11:34:21 +0530

    Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import "dotenv/config"
 import linkRoutes from './routes/route'
+import cors from "cors"
 import redirectRouter from "./routes/redirectUrl";
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1',linkRoutes );
 app.use('/', redirectRouter)
