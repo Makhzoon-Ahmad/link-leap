c7a9ad15e9d1d20eb5211874739583b6ba483107
Makhzoon <amakhzoon10@gmail.com>
2025-03-21 11:46:58 +0530

    Update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,10 @@ import cors from "cors"
 import redirectRouter from "./routes/redirectUrl";
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin : ["https://link-leap-sigma.vercel.app"],
+    methods: ["POST", "GET"]
+}));
 app.use(express.json());
 app.use('/api/v1',linkRoutes );
 app.use('/', redirectRouter)
